c6b2d1ae698c57cb79c885298e4af4a91df9efef
Simon Wu <mr.wusimon@outlook.com>
2021-06-15 10:57:13 +0800

    use vector array to replay int array

--- guessing_number.cpp
+++ guessing_number.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
+#include <vector> //vector is another kind of array, it's dynamic array intead of statistic array
 #include <string>
 #include <cstdlib>
 #include <ctime>
 using namespace std;
 
-void play_game()
+/* void play_game()
 {
     int guesses[250];
     int guess_count = 0;
@@ -35,6 +36,51 @@ void play_game()
         else
         {
             cout << "the number is high" << endl;
+        }
+    }
+} */
+
+void print_vector(vector<int> data)
+{
+    for (int i = 0; i < data.size(); i++)
+    {
+        cout << data[i] << "\t";
+    }
+    cout << endl;
+    cout << endl;
+    cout << "You guessed " << data.size() << " times in total." << endl;
+    cout << "The given random number is " << data[data.size() - 1] << "." << endl;
+}
+
+void play_game()
+{
+    vector<int> guesses;
+    int random = rand() % 1000;
+    int guess_number;
+
+    while (true)
+    {
+        cout << "Guess a number: ";
+        cin >> guess_number;
+        guesses.push_back(guess_number);
+
+        if (guess_number == random)
+        {
+            cout << endl;
+            cout << "Cool, You won!" << endl;
+            cout << endl;
+            print_vector(guesses);
+
+            cout << endl;
+            break;
+        }
+        else if (guess_number < random)
+        {
+            cout << "Too low" << endl;
+        }
+        else
+        {
+            cout << "Too high" << endl;
         }
     }
 }
